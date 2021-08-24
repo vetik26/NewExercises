ae1e6ff0ed2265d3187a755713e8a187ce72afe1
vetik26 <vitalikupenieks@gmail.com>
2021-08-25 00:30:06 +0300

    fixed ex4 flow of control

--- flow-of-control/ex4.php
+++ flow-of-control/ex4.php
@@ -1,13 +1,38 @@
 <?php
 
-$dayNumber = [0, 1 ,2 ,3 ,4 ,5 ,6 ];
-$days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
-];
-$number = (int) readline(" Enter the of number: ");
 
-switch ($number = $days)
+$dayNumber = (int) readline(" Enter the of number: ");
+
+switch ($dayNumber)
 {
-    case  $dayNumber = $days:
-        echo 'susafasf';
+    case $dayNumber ===  0:
+        echo 'Sunday' . PHP_EOL;
+        break;
 
+    case $dayNumber === 1:
+        echo 'Monday' . PHP_EOL;
+        break;
+
+    case $dayNumber === 2:
+        echo 'Tuesday'. PHP_EOL;
+        break;
+
+    case $dayNumber === 3:
+        echo 'Wednesday' . PHP_EOL;
+        break;
+
+    case $dayNumber === 4:
+        echo 'Thursday'. PHP_EOL;
+        break;
+
+    case $dayNumber === 5:
+        echo 'Friday'. PHP_EOL;
+        break;
+
+    case $dayNumber === 6:
+        echo 'Saturday'. PHP_EOL;
+        break;
+
+    default:
+       echo 'Not a valid day, try again'. PHP_EOL;
 }

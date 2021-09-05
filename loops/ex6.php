f364fcb85d20c0c266f3c333b1b3019150b5dc57
vetik26 <vitalikupenieks@gmail.com>
2021-09-06 00:46:25 +0300

    fixed ex6 in loops

--- loops/ex6.php
+++ loops/ex6.php
@@ -1,7 +1,26 @@
 <?php
 
-$rows = readline('Enter the number of rows: ');
-$stars = '********';
-$branch = '////////\\\\\\\\\\\\\\\\';
 
-echo $stars . $stars;
+$lenght  = 0;
+$number = (int) readline('Number of rows: ');
+
+for($i = 1; $i<$number; $i++)
+    {
+       $lenght = $lenght + 8;
+    }
+
+for($i = 8; $i <= $lenght; $i = $i + 8)
+{
+
+    if($i === 8)
+        {
+            echo str_repeat('/', $lenght/2);
+            echo str_repeat('\\', $lenght/2);
+            echo PHP_EOL;
+        }
+             echo str_repeat('/', ($lenght - $i) / 2);
+             echo str_repeat('*', $i);
+             echo str_repeat('\\', ($lenght - $i) / 2);
+             echo PHP_EOL;
+
+}
